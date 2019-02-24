4b6ea6cd715e2405d83c5cfd87376b9ecf9018ed
Jay Bisa <jay.bisa@gmail.com>
2019-02-24 16:56:39 -0500

    Need an example for async multithreaded program.

--- MultiThreading/MakeBreakfast.cs
+++ MultiThreading/MakeBreakfast.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using MultiThreading.Factories;
 using MultiThreading.Models;
+using System.Threading.Tasks;
 
 namespace MultiThreading
 {
@@ -14,8 +15,14 @@ namespace MultiThreading
 
             var chef = new Chef("Jay", breakfastAlgorithm);
 
+            // Sync vs. async
             chef.PrepareBreakfast();
             await chef.PrepareBreakfastAsync();
+
+            // Async multithreaded
+            var assistantChef = new Chef("Chris", breakfastAlgorithm);
+
+            //Parallel.For()
 
             Console.WriteLine("End of program...");
         }
